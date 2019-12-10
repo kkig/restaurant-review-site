e8b332f51ee0d4ce55f60d793a693a63fe4018c6
kkig <kigoshi.kanazawa@gmail.com>
2019-12-10 16:25:08 +0100

    usePlaces refactor

---
+++ src/APIs/usePlaces.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+//import { usePosition } from './usePosition';
+
+import GOOGLE_MAP_API_KEY from './GoogleMapKey';
+
+import ShopDataItem from '../components/ShopDataItemClass';
+//import StoreContext from '../stores/StoreContext';
+//import StoreContext from '../stores/StoreContext';
+
+export const usePlaces = (latitude, longitude) => {
+    //const { latitude, longitude } = usePosition();
+    const [ placesData, setPlaces ] = useState({});
+    const [ formatChanged, setFormat ] = useState(false);
+    const [ isPlaceRequested, setPlaceRequested ] = useState(false);
+
+    //const store = useContext(StoreContext);
+
+    const getPlacesData = () => {
+        if(isPlaceRequested || placesData.results) {
+            return;
+        }
+
+        const fetchData = () => {
+            const endpoint = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${GOOGLE_MAP_API_KEY}`;
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+        
+            fetch(proxy + endpoint)
+                .then(res => res.json())
+                .then(data => {
+                    setPlaces(data)   
+                })
+                .catch(error => console.log(error));
+            console.log('Place fetched');
+        }
+        fetchData();
+        setPlaceRequested(true);
+    };
+
+    const formatData = data => {
+        let newArray = [];
+        data.map(restaurant => newArray.push(new ShopDataItem(
+            restaurant.place_id, 
+            restaurant.name, 
+            restaurant.types[0], 
+            restaurant.vicinity, 
+            restaurant.geometry.location.lat,
+            restaurant.geometry.location.lng,
+            restaurant.rating,
+            [],
+            'GOOGLE',
+        )));
+
+        setPlaces(newArray);
+        setFormat(true);
+        console.log('Formatted')
+    };
+
+    useEffect(() => {
+        if(placesData.results) {
+            formatData(placesData.results);
+        } 
+    }, [placesData]);
+
+    //formatChanged && console.log(placesData);
+
+    //latitude && longitude && console.log('usePlace ready. Lat: ' + latitude)
+    //if(store != null && store.userLocation.lat !== undefined)  {console.log('ready to fetch')};
+    //latitude && longitude && !isPlaceRequested && getPlacesData();
+    latitude !== undefined && longitude !== undefined && !isPlaceRequested && getPlacesData();
+    //!isPlaceRequested && getPlacesData();
+    
+    return { placesData, formatChanged };
+}
