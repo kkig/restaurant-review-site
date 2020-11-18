789144d7accfebf6d8e5b0e4d515fc4aa189664d
kkig <kigoshi.kanazawa@gmail.com>
2020-11-18 14:36:45 +0900

    reset to stable version before yarn upgrade & cleaned console

--- src/components/LocationDialog/usePlaces.js
+++ src/components/LocationDialog/usePlaces.js
@@ -1,64 +1,69 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // Class
-import ShopDataItem from '../../classes/ShopDataItemClass';
+import ShopDataItem from "../../classes/ShopDataItemClass";
 
 export const usePlaces = (latitude, longitude) => {
-    const [ placesData, setPlaces ] = useState({});
-    const [ formatChanged, setFormat ] = useState(false);
-    const [ isPlaceRequested, setPlaceRequested ] = useState(false);
+  const [placesData, setPlaces] = useState({});
+  const [formatChanged, setFormat] = useState(false);
+  const [isPlaceRequested, setPlaceRequested] = useState(false);
 
-    const getPlacesData = () => {
-        if(!!isPlaceRequested || placesData.results) {
-            return;
-        }
+  const getPlacesData = () => {
+    if (!!isPlaceRequested || placesData.results) {
+      return;
+    }
 
-        const fetchData = () => {
-            const GOOGLE_MAP_API_KEY = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_GOOGLE_KEY : process.env.REACT_APP_DEV_GOOGLE_KEY;
-            const endpoint = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${GOOGLE_MAP_API_KEY}`;
-            const proxy = `https://cors-anywhere.herokuapp.com/`;
-        
-            fetch(proxy + endpoint)
-                .then(res => res.json())
-                .then(data => {
-                    setPlaces(data)   
-                })
-                .catch(error => console.log(error));
-            console.log('Place fetched');
-        }
-        fetchData();
-        setPlaceRequested(true);
+    const fetchData = () => {
+      const GOOGLE_MAP_API_KEY =
+        process.env.NODE_ENV === "production"
+          ? process.env.REACT_APP_PROD_GOOGLE_KEY
+          : process.env.REACT_APP_DEV_GOOGLE_KEY;
+      const endpoint = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${GOOGLE_MAP_API_KEY}`;
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
+
+      fetch(proxy + endpoint)
+        .then((res) => res.json())
+        .then((data) => {
+          setPlaces(data);
+        })
+        .catch((error) => console.log(error));
     };
+    fetchData();
+    setPlaceRequested(true);
+  };
 
-    const formatData = data => {
-        let newArray = [];
-        data.map(restaurant => newArray.push(
-            new ShopDataItem(
-
-            restaurant.place_id, 
-            restaurant.name, 
-            restaurant.types[0], 
-            restaurant.vicinity, 
-            restaurant.geometry.location.lat,
-            restaurant.geometry.location.lng,
-            restaurant.rating,
-            [],
-            'GOOGLE',
+  const formatData = (data) => {
+    let newArray = [];
+    data.map((restaurant) =>
+      newArray.push(
+        new ShopDataItem(
+          restaurant.place_id,
+          restaurant.name,
+          restaurant.types[0],
+          restaurant.vicinity,
+          restaurant.geometry.location.lat,
+          restaurant.geometry.location.lng,
+          restaurant.rating,
+          [],
+          "GOOGLE"
         )
-        ));
+      )
+    );
 
-        setPlaces(newArray);
-        setFormat(true);
-        console.log('Formatted')
-    };
+    setPlaces(newArray);
+    setFormat(true);
+  };
 
-    useEffect(() => {
-        if(placesData.results) {
-            formatData(placesData.results);
-        } 
-    }, [placesData]);
+  useEffect(() => {
+    if (placesData.results) {
+      formatData(placesData.results);
+    }
+  }, [placesData]);
 
-    latitude !== undefined && longitude !== undefined && !isPlaceRequested && getPlacesData();
-    
-    return { placesData, formatChanged };
-}
+  latitude !== undefined &&
+    longitude !== undefined &&
+    !isPlaceRequested &&
+    getPlacesData();
+
+  return { placesData, formatChanged };
+};
--- src/stores/RestourantStores.js
+++ src/stores/RestourantStores.js
@@ -1,49 +1,33 @@
-import React from 'react';
-import { useLocalStore } from 'mobx-react';
+import React from "react";
+import { useLocalStore } from "mobx-react";
 
-import StoreContext from './StoreContext';
+import StoreContext from "./StoreContext";
 
 const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => ({
+    ShopDataItem: [],
+    addNewShop: (newShop) => {
+      store.ShopDataItem.unshift(newShop);
+    },
+    get countData() {
+      return store.ShopDataItem.length;
+    },
+    addNewComment: (id, newComment) => {
+      store.ShopDataItem.filter(
+        (shop) => shop.id === id && shop.ratings.unshift(newComment)
+      );
+      //console.log(updated);
+    },
+    userLocation: [],
+    addUserLocation: (lat, lng) => {
+      store.userLocation = { ...store.userLocation, lat: lat, lng: lng };
+    },
+    clickedLocation: [],
+  }));
 
-    const store = useLocalStore(() => ({
-        ShopDataItem: [],
-        addNewShop: newShop => {
-            store.ShopDataItem.unshift(newShop);
-        },
-        get countData() {
-            return store.ShopDataItem.length;
-        },
-        addNewComment: (id, newComment) => {
-            store.ShopDataItem.filter(shop => (
-                shop.id === id && (
-                    shop.ratings.unshift(newComment))
-                )
-            );
-            //console.log(updated);
-        },
-        userLocation: [],
-        addUserLocation: (lat, lng) => {
-            store.userLocation = {...store.userLocation, lat: lat, lng: lng };
-            console.log('Location stored');
-        },
-        clickedLocation: [],
-        /*
-        storeClickedLocation: (id, lat, lng, address) => {
-            store.clickedLocation = {
-                ...store.clickedLocation, 
-                id: id, 
-                lat: lat, 
-                lng: lng, 
-                address: address
-            };
-            console.log(store.clickedLocation)
-        } 
-        */
-    }));
-
-    return (
-        <StoreContext.Provider value={store}>{ children }</StoreContext.Provider>
-    );
-}
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
 
 export default StoreProvider;
